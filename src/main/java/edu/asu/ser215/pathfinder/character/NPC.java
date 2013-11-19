d4506487624026665bd679d7dcedd49d79b93979
Seth Moon <seth@d3x.co>
2013-11-19 10:42:08 -0700

    Adding some character information.

---
+++ src/main/java/edu/asu/ser215/pathfinder/character/NPC.java
@@ -0,0 +1,14 @@
+package edu.asu.ser215.pathfinder.character;
+
+public class NPC extends AbstractCharacter {
+
+	public NPC(String name) {
+		super(name);
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+
+}
---
+++ src/main/java/edu/asu/ser215/pathfinder/character/Player.java
@@ -0,0 +1,14 @@
+package edu.asu.ser215.pathfinder.character;
+
+public class Player extends AbstractCharacter {
+
+	public Player(String name) {
+		super(name);
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+
+}
